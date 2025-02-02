d360fc2112d5cb290f50ba3ac6e740eafa4baef0
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2025-02-02 17:20:54 +0000

    initial commit & project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# gl-gfx
+
+### References
+
+- [Glitter](https://github.com/Polytonic/Glitter)
---
+++ src/Headers/gl-gfx.hpp
@@ -0,0 +1,18 @@
+#ifndef GL_GFX
+#define GL_GFX
+
+#pragma once
+
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <btBulletDynamicsCommon.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+
+const int mWidth = 1280;
+const int mHeight = 800;
+
+#endif
