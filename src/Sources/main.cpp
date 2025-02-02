41a454c71110bed1a7cde6340d8257ad42920d22
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2025-02-02 17:34:55 +0000

    tidy

--- src/Sources/main.cpp
+++ src/Sources/main.cpp
@@ -8,35 +8,35 @@
 
 int main(int argc, char *argv[]) {
   glfwInit();
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-  auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
-  // Check for Valid Context
-  if (mWindow == nullptr) {
+  GLFWwindow *window =
+      glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+
+  if (window == nullptr) {
     fprintf(stderr, "Failed to Create OpenGL Context");
     return EXIT_FAILURE;
   }
 
-  // Create Context and Load OpenGL Functions
-  glfwMakeContextCurrent(mWindow);
+  glfwMakeContextCurrent(window);
   gladLoadGL();
-  fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-  // Rendering Loop
-  while (glfwWindowShouldClose(mWindow) == false) {
-    if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-      glfwSetWindowShouldClose(mWindow, true);
+  fprintf(stdout, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    // Background Fill Color
+  while (glfwWindowShouldClose(window) == false) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
+
     glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Flip Buffers and Draw
-    glfwSwapBuffers(mWindow);
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
