8c6636afeb32e95cf4f3a507f2d3796313705ef4
Darragh McGurk <120114728+dmcg310@users.noreply.github.com>
2025-02-08 16:23:36 +0000

    feat: framebuffer resize callback

--- src/Sources/main.cpp
+++ src/Sources/main.cpp
@@ -1,19 +1,34 @@
 #include "gl_gfx.hpp"
 #include "window.hpp"
 
-int main(int argc, char *argv[]) {
-  Window window(1600, 900, "gl-gfx");
+#include <GLFW/glfw3.h>
+
+// TODO: read from config file?
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEGIHT 900
+
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int w,
+                               int h);
+
+int main(void) {
+  Window window(WINDOW_WIDTH, WINDOW_HEGIHT, "gl-gfx");
 
   gladLoadGL();
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEGIHT);
+  glfwSetFramebufferSizeCallback(window.get(), framebuffer_size_callback);
 
   while (!window.should_close()) {
-    window.poll_events();
-
     glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     window.swap_buffers();
+    window.poll_events();
   }
 
   return 0;
+}
+
+void framebuffer_size_callback([[maybe_unused]] GLFWwindow *window, int w,
+                               int h) {
+  glViewport(0, 0, w, h);
 }
